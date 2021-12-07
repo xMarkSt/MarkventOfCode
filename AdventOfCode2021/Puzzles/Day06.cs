8f979e83c0ddcfcecf5be2fa5b7f340ee9858610
xMarkSt <=>
2021-12-07 06:42:54 +0100

    Day 7

---
+++ AdventOfCode2021/Puzzles/Day06.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Puzzles
+{
+    public class Day06 : AocPuzzle
+    {
+        public override int Year => 2021;
+        public override int Day => 6;
+        protected override void Solve(IEnumerable<string> input)
+        {
+            var inp = input.ToList();
+            List<int> fishes = inp[0].Split(',').Select(int.Parse).ToList();
+            var fishesCount = new Dictionary<int, long>();
+            
+            // Fill dictionary with 0-8 keys and count 0.
+            for (int i = 0; i <= 8; i++)
+            {
+                fishesCount.Add(i, 0);
+            }
+            
+            // Load data
+            foreach (int fish in fishes)
+            { 
+                fishesCount[fish]++;
+            }
+            
+            PartOne = Simulate(fishesCount, 80).ToString();
+            PartTwo = Simulate(fishesCount, 256).ToString();
+        }
+
+        private long Simulate(Dictionary<int, long> startData, int maxDays)
+        {
+            int days = 0;
+            while (days < maxDays)
+            {
+                long zeroCount = startData[0];
+
+                for (int day = 1; day <= 8; day++)
+                {
+                    startData[day-1] = startData[day];
+                }
+   
+                startData[6] += zeroCount;
+                startData[8] = zeroCount;
+                days++;
+            }
+            return startData.Values.Sum();
+        }
+    }
+}
---
+++ AdventOfCode2021/Puzzles/Day07.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2021.Puzzles;
+
+public class Day07 : AocPuzzle
+{
+    public override int Year => 2021;
+    public override int Day => 7;
+    protected override void Solve(IEnumerable<string> input)
+    {
+        var inp = input.ToList();
+        List<int> positions = inp[0].Split(',').Select(int.Parse).ToList();
+        long lowest = long.MaxValue;
+        long lowest2 = long.MaxValue;
+        for (int position = 0; position < positions.Max(); position++)
+        {
+            long count = 0;
+            long count2 = 0;
+            foreach (int position2 in positions)
+            {
+                long diff = Math.Abs(position - position2);
+                count += diff;
+                for (int i = 1; i <= diff; i++)
+                {
+                    count2 += i;
+                }
+            }
+            if (count < lowest) lowest = count;
+            if (count2 < lowest2) lowest2 = count2;
+        }
+        PartOne = lowest.ToString();
+        PartTwo = lowest2.ToString();
+    }
+}
