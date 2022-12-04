9e89ff1bc1b3dacc59bd959474da4ad5643dbb50
xMarkSt <=>
2022-12-04 08:09:04 +0100

    Day4

--- AdventOfCode2021/Puzzles/2022/Day04.cs
+++ AdventOfCode2021/Puzzles/2022/Day04.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2021.Utils;
 
 namespace AdventOfCode2021.Puzzles._2022
 {
@@ -6,9 +8,36 @@ namespace AdventOfCode2021.Puzzles._2022
     {
         public override int Year => 2022;
         public override int Day => 4;
+
         protected override void Solve(IEnumerable<string> input)
         {
-            throw new System.NotImplementedException();
+            var list = input.Select(x => x.Split(',')
+                .Select(y => y.Split('-').AsIntList().ToList()));
+            int count = 0;
+            int count2 = 0;
+            foreach ((List<int> first, List<int> second, _) in list)
+            {
+                // part 1
+                if (first[0] <= second[0] && second[0] <= first[1] &&
+                    first[0] <= second[1] && second[1] <= first[1] ||
+                    second[0] <= first[0] && first[0] <= second[1] &&
+                    second[0] <= first[1] && first[1] <= second[1])
+                {
+                    count++;
+                }
+
+                // part 2
+                if (first[0] <= second[0] && second[0] <= first[1] ||
+                    first[0] <= second[1] && second[1] <= first[1] ||
+                    second[0] <= first[0] && first[0] <= second[1] ||
+                    second[0] <= first[1] && first[1] <= second[1])
+                {
+                    count2++;
+                }
+            }
+
+            PartOne = count;
+            PartTwo = count2;
         }
     }
 }
---
+++ AdventOfCode2021/Puzzles/2022/Day05.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2021.Puzzles._2022
+{
+    public class Day05 : AocPuzzle
+    {
+        public override int Year => 2022;
+
+        public override int Day => 5;
+
+        protected override void Solve(IEnumerable<string> input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
