71a9a2bc19e80e3dd58114f596d3e2c20b55d0d8
xMarkSt <=>
2022-12-05 11:04:48 +0100

    Refactor day 3

--- AdventOfCode2021/Puzzles/2022/Day03.cs
+++ AdventOfCode2021/Puzzles/2022/Day03.cs
@@ -11,26 +11,20 @@ namespace AdventOfCode2021.Puzzles._2022
 
         protected override void Solve(IEnumerable<string> input)
         {
-            int prioSum = 0;
-            foreach (string s in input)
-            {
-                // First half
-                string c1 = s[..(s.Length / 2)];
-                // Second half
-                string c2 = s[(s.Length / 2)..];
-                char both = c1.Intersect(c2).First();
-                prioSum += both >= 'a' ? both - 'a' + 1 : both - 'A' + 27;
-            }
-
-            PartOne = prioSum;
-
-            prioSum = 0;
-            foreach (string[] s in input.Chunk(3))
-            {
-                char both = s[0].Intersect(s[1]).Intersect(s[2]).First();
-                prioSum += both >= 'a' ? both - 'a' + 1 : both - 'A' + 27;
-            }
-            PartTwo = prioSum;
+            PartOne = input
+                .Select(s => s[..(s.Length / 2)]
+                    .Intersect(s[(s.Length / 2)..])
+                    .First())
+                .Select(c => c >= 'a' ? c - 'a' + 1 : c - 'A' + 27)
+                .Sum();
+            
+            PartTwo = input.Chunk(3)
+                .Select(s => 
+                    s[0].Intersect(s[1])
+                        .Intersect(s[2])
+                        .First())
+                .Select(c => c >= 'a' ? c - 'a' + 1 : c - 'A' + 27)
+                .Sum();
         }
     }
 }
