8aa44ad1848a18d3e17583b6b1ea5c1404573067
xMarkSt <=>
2022-12-05 10:33:36 +0100

    Solve Day 5, refactor day 3

--- AdventOfCode2021/Puzzles/2022/Day03.cs
+++ AdventOfCode2021/Puzzles/2022/Day03.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace AdventOfCode2021.Puzzles._2022
 {
@@ -7,106 +8,29 @@ namespace AdventOfCode2021.Puzzles._2022
     {
         public override int Year => 2022;
         public override int Day => 3;
+
         protected override void Solve(IEnumerable<string> input)
         {
             int prioSum = 0;
-            foreach (var s in input)
+            foreach (string s in input)
             {
-                string c1 = s.Substring(0, s.Length / 2);
-                string c2 = s.Substring(s.Length / 2);
-                char? both = FindContainsBoth(c1, c2);
-                if (both.HasValue)
-                {
-                    int prio;
-                    // Small
-                    if (both >= 'a')
-                    {
-                        prio = (int)(both - 'a' + 1);
-                    }
-                    else
-                    {
-                        prio = (int)(both - 'A' + 27);
-                    }
-
-                    prioSum += prio;
-                }
-                else
-                {
-                    Debug.Write("No letter in both comopartments?");
-                }
+                // First half
+                string c1 = s[..(s.Length / 2)];
+                // Second half
+                string c2 = s[(s.Length / 2)..];
+                char both = c1.Intersect(c2).First();
+                prioSum += both >= 'a' ? both - 'a' + 1 : both - 'A' + 27;
             }
 
             PartOne = prioSum;
 
             prioSum = 0;
-            string s1 = "";
-            string s2 = "";
-            string s3 = "";
-            int cnt = 0;
-            foreach (string s in input)
+            foreach (string[] s in input.Chunk(3))
             {
-                if (cnt == 0)
-                {
-                    s1 = s;
-                    cnt++;
-                }
-                else if (cnt == 1)
-                {
-                    s2 = s;
-                    cnt++;
-                }
-                else if (cnt == 2)
-                {
-                    s3 = s;
-                    char? both = FindContainsThree(s1, s2, s3);
-                    if (both.HasValue)
-                    {
-                        int prio;
-                        // Small
-                        if (both >= 'a')
-                        {
-                            prio = (int)(both - 'a' + 1);
-                        }
-                        else
-                        {
-                            prio = (int)(both - 'A' + 27);
-                        }
-
-                        prioSum += prio;
-                        cnt = 0;
-                    }
-                    else
-                    {
-                        Debug.Write("No letter in both comopartments?");
-                    }
-
-                }
+                char both = s[0].Intersect(s[1]).Intersect(s[2]).First();
+                prioSum += both >= 'a' ? both - 'a' + 1 : both - 'A' + 27;
             }
-
             PartTwo = prioSum;
-        }
-
-        char? FindContainsBoth(string a, string b)
-        {
-            foreach (char c1 in a)
-            foreach (char c2 in b)
-            {
-                if (c1 == c2) return c1;
-            }
-
-            return null;
-        }
-        
-        char? FindContainsThree(string a, string b, string c)
-        {
-            foreach (char c1 in a)
-            foreach (char c2 in b)
-            foreach (char c3 in c)
-            {
-                if (c1 == c2 && c1 == c3) return c1;
-            }
-
-            return null;
         }
     }
 }
--- AdventOfCode2021/Puzzles/2022/Day05.cs
+++ AdventOfCode2021/Puzzles/2022/Day05.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using AdventOfCode2021.Utils;
 
 namespace AdventOfCode2021.Puzzles._2022
 {
@@ -10,7 +14,64 @@ namespace AdventOfCode2021.Puzzles._2022
 
         protected override void Solve(IEnumerable<string> input)
         {
-            throw new System.NotImplementedException();
+            var (stacksStrings, instructions, _) = input.SplitByEmptyLine().Select(x => x.ToList());
+            var stacks = new List<Stack<char>>();
+            var stacks2 = new List<Stack<char>>();
+            // Add the stacks to the list
+            for (int i = 1; i < stacksStrings[^1].Length; i += 4)
+            {
+                stacks.Add(new Stack<char>());
+                stacks2.Add(new Stack<char>());
+            }
+            
+            // Fill the stacks
+            for (int i = stacksStrings.Count - 2; i >= 0; i--)
+            {
+                string stack = stacksStrings[i];
+                int stackIndex = 0;
+                for (int j = 1; j < stack.Length; j += 4)
+                {
+                    char c = stack[j];
+                    if (char.IsLetter(c))
+                    {
+                        stacks[stackIndex].Push(c);
+                        stacks2[stackIndex].Push(c);
+                    }
+                    stackIndex++;
+                }
+            }
+            
+            var regex = new Regex(@"move (\d+) from (\d) to (\d)");
+
+            foreach (string instruction in instructions)
+            {
+                (string amount, string from, string to, _) = InputUtils.RegexGroupCapture(regex, instruction);
+                int fromIndex = int.Parse(from) - 1;
+                int toIndex = int.Parse(to) - 1;
+                
+                // Part one
+                for (int i = 0; i < int.Parse(amount); i++)
+                {
+                    var crate = stacks[fromIndex].Pop();
+                    stacks[toIndex].Push(crate);
+                }
+                
+                // Part two
+                var crates = new StringBuilder();
+                for (int i = 0; i < int.Parse(amount); i++)
+                {
+                    crates.Append(stacks2[fromIndex].Pop());
+                }
+
+                for (int i = crates.Length - 1; i >= 0; i--)
+                {
+                    stacks2[toIndex].Push(crates[i]);
+                }
+            }
+            
+            // Top of each stack
+            PartOne = stacks.Aggregate("", (current, stack) => current + stack.Peek());
+            PartTwo = stacks2.Aggregate("", (current, stack) => current + stack.Peek());
         }
     }
 }
