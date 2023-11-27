3d31699621cc9e910747e73a1a6ef065c26d3c32
Mark Stam <markstam099@gmail.com>
2023-11-27 19:08:00 +0100

    Update for 2023

--- AdventOfCode2021/Puzzles/2022/Day07.cs
+++ AdventOfCode2021/Puzzles/2022/Day07.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using AD;
 using AdventOfCode2021.Utils;
 
 namespace AdventOfCode2021.Puzzles._2022;
---
+++ AdventOfCode2021/Puzzles/2023/Day01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Puzzles._2023
+{
+    public class Day01 : AocPuzzle
+    {
+        public override int Year => 2023;
+
+        public override int Day => 1;
+
+        protected override void Solve(IEnumerable<string> input)
+        {
+            
+        }
+    }
+}
